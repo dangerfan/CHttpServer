1c6987f840d37d0614dce11efd08e6aa74ff690b
dangerfan <jared.dziedzic@gmail.com>
2026-08-08 18:21:57 -0400

    Updates

--- tests/headers.c
+++ tests/headers.c
@@ -7,25 +7,25 @@
 
 int main() {
 
-    const char *raw_request =
+    const char *raw_request_good =
         "GET /index.html HTTP/1.1\r\n"
         "Host: localhost:8080\r\n"
         "User-Agent: curl/7.68.0\r\n"
         "Accept: */*\r\n"
-        "Yo: Hey\r\n"
         "\r\n";
-
-
-    http_request request = {0};
-
-    parse_http_headers(raw_request, &request);
-
-    printf("Parsed HTTP Headers:\n");
-    for (size_t i = 0; i < request.header_count; i++) {
-        printf("%s: %s\n", request.headers[i].key, request.headers[i].value);
+    
+    http_request request_good = {0};
+    
+    if (parse_http_headers(raw_request_good, &request_good) != HTTP_PARSE_OK) {
+        printf("Parsing good headers failed.\n");
     }
 
-    free_http_headers(&request);
+    printf("Parsed HTTP Headers:\n");
+    for (size_t i = 0; i < request_good.header_count; i++) {
+        printf("%s: %s\n", request_good.headers[i].key, request_good.headers[i].value);
+    }
+
+    free_http_headers(&request_good);
 
     return 0;
 }
